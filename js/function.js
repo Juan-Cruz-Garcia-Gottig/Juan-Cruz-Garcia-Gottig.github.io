9956f1efb2cd1ec86eb9b78b94fe7dbd06833822
Juan Cruz Garcia Gottig <juancruzgarciag@gmail.com>
2023-02-24 10:49:10 -0300

    seguimos aprendiendo

--- js/function.js
+++ js/function.js
@@ -1,1 +1,12 @@
 console.log("Hola Mundo");
+
+let n1 = 10;
+let n2 = 3;
+let resultado = n1 + n2;
+
+console.log("La suma es: " + resultado);
+
+document.getElementById("boton").onclick = function (){
+    console.log("Capturamos el evento click")
+    document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en JS"
+}
