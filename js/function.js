abce3dac23b0eb75e3dbf8bfc19e5896cbdb5a11
Juan Cruz Garcia Gottig <juancruzgarciag@gmail.com>
2023-02-25 20:39:18 -0300

    aprendiendo diferentes formas de cambios

--- js/function.js
+++ js/function.js
@@ -1,3 +1,4 @@
+/**
 console.log("Hola Mundo");
 
 let n1 = 10;
@@ -10,3 +11,22 @@ document.getElementById("boton").onclick = function (){
     console.log("Capturamos el evento click")
     document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en JS"
 }
+
+document.addEventListener("click", function () {
+    console.log("Hola Mundo desde Event Listener")
+    document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en JS"
+})
+*/
+
+document.getElementById("boton").addEventListener("click", function () {
+    console.log("Hola Mundo desde Event Listener")
+    document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en JS"
+})
+
+document.getElementById("boton_color").addEventListener("click", function () {
+    document.body.style.backgroundColor = "#DFCB24";
+})
+
+document.getElementById("boton_default").addEventListener("click", function () {
+    document.body.style.backgroundColor = "aqua";
+})
