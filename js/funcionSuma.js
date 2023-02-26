045ba5d5e4f70d880b6a4de1c22675b74d0a00f7
Juan Cruz Garcia Gottig <juancruzgarciag@gmail.com>
2023-02-26 00:24:11 -0300

    cambios

--- js/funcionSuma.js
+++ js/funcionSuma.js
@@ -1,14 +1,14 @@
 
-document.getElementById("sumar").addEventListener('click', function(){
-    let numeroA = document.getElementById('n1').value;
-    console.log('El valor del número es: '+numeroA);
-    let numeroB = document.getElementById('n1').value;
-    console.log('El valor del número es: '+numeroB);
-    let resultado = sumar(parseInt(numeroA) + parseInt(numeroB));
-    console.log('El resultado de la suma es: '+resultado);
+document.getElementById("sumar").addEventListener("click", function(){
+    let numeroA = document.getElementById("n1").value;
+    console.log("El valor del número es: "+numeroA);
+    let numeroB = document.getElementById("n2").value;
+    console.log("El valor del número es: "+numeroB);
+    let resultado = sumar(parseInt(numeroA), parseInt(numeroB));
+    console.log("El resultado de la suma es: "+resultado);
 
-    document.getElementById('resultado').innerHTML = resultado;
-    document.getElementById('contenedorResultado').style.display = 'block';
+    document.getElementById("resultado").innerHTML = resultado;
+    document.getElementById("contenedorResultado").style.display = "block";
 });
 
 function sumar(a,b){
