e16792c9a71eb02e813f21aa69ced9ea4de87409
Juan Cruz Garcia Gottig <juancruzgarciag@gmail.com>
2023-03-04 00:37:34 -0300

    trabajo finalizado

--- Proyecto-final/JS/funciones.js
+++ Proyecto-final/JS/funciones.js
@@ -26,3 +26,59 @@ function Exp() {
 function Ap() {
     document.getElementById("aprendizaje").style.display="block"
 }
+function Pres() {
+    var x = document.getElementById("presentacion");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+function Nya() {
+    var x = document.getElementById("nya");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+function Fecha() {
+    var x = document.getElementById("fecha");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+function Dom() {
+    var x = document.getElementById("domicilio");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+function Mail() {
+    var x = document.getElementById("email");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+function Exp() {
+    var x = document.getElementById("exp");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+function Ap() {
+    var x = document.getElementById("aprendizaje");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
