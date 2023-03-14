7b01c27906c513def4fec164fc681489bec154d1
juan-cruz-garcia-gottig <126935330+juan-cruz-garcia-gottig@users.noreply.github.com>
2023-03-13 23:09:57 -0300

    initial commit

---
+++ funciones.js
@@ -0,0 +1,84 @@
+document.getElementById("lista-pre").addEventListener("click", Pres);
+document.getElementById("lista-nya").addEventListener("click", Nya);
+document.getElementById("lista-fecha").addEventListener("click", Fecha);
+document.getElementById("lista-dom").addEventListener("click", Dom);
+document.getElementById("lista-email").addEventListener("click", Mail);
+document.getElementById("lista-exp").addEventListener("click", Exp);
+document.getElementById("lista-ap").addEventListener("click", Ap);
+function Pres() {
+    document.getElementById("presentacion").style.display="none"
+}
+function Nya() {
+    document.getElementById("nya").style.display="block"
+}
+function Fecha() {
+    document.getElementById("fecha").style.display="block"
+}
+function Dom() {
+    document.getElementById("domicilio").style.display="block"
+}
+function Mail() {
+    document.getElementById("email").style.display="block"
+}
+function Exp() {
+    document.getElementById("exp").style.display="block"
+}
+function Ap() {
+    document.getElementById("aprendizaje").style.display="block"
+}
+function Pres() {
+    var x = document.getElementById("presentacion");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+function Nya() {
+    var x = document.getElementById("nya");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+function Fecha() {
+    var x = document.getElementById("fecha");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+function Dom() {
+    var x = document.getElementById("domicilio");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+function Mail() {
+    var x = document.getElementById("email");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+function Exp() {
+    var x = document.getElementById("exp");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+function Ap() {
+    var x = document.getElementById("aprendizaje");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
